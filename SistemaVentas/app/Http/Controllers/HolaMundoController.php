7c19e27cd1bafaeab926a302609ae181f7661707
Javier Galdámez <javier.galdamez@creativaconsultores.com>
2017-10-22 18:33:39 -0600

    :tv: Segunda practica con enrutamiento para acceder al método de un controlador

---
+++ SistemaVentas/app/Http/Controllers/HolaMundoController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace SistemaVentas\Http\Controllers;
+
+class HolaMundoController extends Controller
+{
+	public function saludo() {
+		return "Hola Mundo!";
+	}
+}
--- SistemaVentas/routes/web.php
+++ SistemaVentas/routes/web.php
@@ -11,22 +11,5 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get("/primera/ruta", function () {
-    return "Hola Mundo!";
-});
-
-Route::get("publicacion/{publicaciones}/comentario/{comentarios}", function ($publicacionId, $comentarioId) {
-    return "<h1>Publicación número:</h1><i>".$publicacionId."</i><br><h1>Comentario número:</h1><i>".$comentarioId."</i>";
-});
-
-Route::get("/suma/{numero1?}/{numero2?}", function ($numero1 = 1, $numero2 = 1) {
-    return $numero1 + $numero2;
-})->where(["numero1" => "\d+", "numero2" => "\d+"]);
-
-Route::post("/saludo/{nombre?}", function ($nombre = "usuario") {
-    return "Hola ".$nombre;
-})->where("nombre", "[a-zA-Z]+");	// Restringir los caracteres permitidos del parámetro $nombre mediante una expresión regular
+Route::get("/", function () { return view("welcome"); });
+Route::get("segunda/ruta", "HolaMundoController@saludo");
